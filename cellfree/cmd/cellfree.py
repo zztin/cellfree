dbc8c12ab3ba2f3b62874a8cbced543612ca298e
Taihsiang Ho (tai271828) <e@tai271828.me>
2022-04-08 23:32:45 +0200

    feat(cmd): init click commands

--- cellfree/cmd/cellfree.py
+++ cellfree/cmd/cellfree.py
@@ -1,6 +1,17 @@
-def main():
+import click
+from cellfree.cmd import bam
+from cellfree.cmd import motif
+
+
+@click.group()
+@click.option("--verbose",
+              type=click.Choice(["debug", "info", "warning", "error", "critical"]),
+              default="info",
+              help="Verbose level corresponding to logging level")
+@click.option("--conf", help="Configuration file")
+def main(verbose, conf):
     pass
 
 
-if __name__ == "__main__":
-    main()
+main.add_command(bam.main)
+main.add_command(motif.main)
---
+++ tests/test_cellfree.py
@@ -0,0 +1,16 @@
+import cellfree.cmd.bam as bam
+import cellfree.cmd.motifs as motifs
+
+from click.testing import CliRunner
+
+
+def test_bam():
+    runner = CliRunner()
+    result = runner.invoke(bam.main)
+    assert result.output == "bam command is called\n"
+
+
+def test_motifs():
+    runner = CliRunner()
+    result = runner.invoke(motifs.main)
+    assert result.output == "motif command is called\n"
