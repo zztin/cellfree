5c2b52fffa0073783f562b8fafc1b5ddb7d398aa
Taihsiang Ho (tai271828) <e@tai271828.me>
2022-04-09 19:29:43 +0200

    tmp

--- cellfree/cmd/end_motif.py
+++ cellfree/cmd/end_motif.py
@@ -1,7 +1,8 @@
 import click
+from cellfree.algorithm import end_motif
 
 
 @click.command()
 @click.option("--bamfile", help="Single bamfile")
 def main(bamfile):
-    print("motif command is called")
+    end_motif.run(bamfile)
